90fd107d5382afc69fb706cfa5a8d101cacec025
Akash Morwal <akashbhau101@gmail.com>
2024-10-16 10:17:29 +0530

    My New Project Started using Context Api

---
+++ 10myContextApi/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+// import { Card } from 'reactstrap'
+// import './App.css'
+// import Card from "./components/Card"
+// import { MyThemeProvider } from "./Context/ToggleBtn"
+// import ThemeButton from "./components/ThemeButton"
+
+
+
+function App() {
+  
+  return (
+    // <MyThemeProvider>
+    // <div className="p-10 m-4 grid">
+    <h1 className="p-4">hello Akash This is Practice of Context</h1>
+    // {/* <ThemeButton/>
+    // <Card /> */}
+    // </div>
+    // </MyThemeProvider>
+  )
+}
+
+export default App
---
+++ 10myContextApi/src/Context/ToggleBtn.js
@@ -0,0 +1,13 @@
+// import { createContext, useContext } from "react";
+
+// const MyThemeContext = createContext({
+//     defaultTheme: "Light",
+//     darkTheme: ()=>{},
+//     lightTheme: ()=>{},
+// })
+
+// export const MyThemeProvider = MyThemeContext.Provider 
+
+// export default function useMyTheme(){
+//     return useContext(MyThemeContext)
+// }
